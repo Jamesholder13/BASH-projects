d7d7de56b7f294a6d1790d1fb9db5c65b1c5e68f
Jamesholder13 <72656004+Jamesholder13@users.noreply.github.com>
2022-08-15 11:11:10 -0400

    Add files via upload

---
+++ deleteoldfiles.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#Author: James Holder
+#Date: 08/08/2020
+#Description: This script deletes files older than 60 days
+#Modified: 08/15/2022
+
+#Use find command to display your path of possible old files
+find /home/admin/ -mtime +60 -exec rm {} \;
+if /home/admin/ps >= 0, then
+    echo 'Old files have been deleted!'
+else
+    echo 'There were no old files to delete!'
+fi
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
